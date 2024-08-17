53b715e724a2e8c9a2dca7ca80f3a75a5ca6ae62
wilcab21 <cabwiltr@gmail.com>
2024-08-16 18:44:33 -0600

    Configuración de micrófono

--- app/src/main/java/firebase/app/entrevista/MainActivity.java
+++ app/src/main/java/firebase/app/entrevista/MainActivity.java
@@ -1,9 +1,15 @@
 package firebase.app.entrevista;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -22,6 +28,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +36,9 @@ import firebase.app.entrevista.model.Persona;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaRecorder grabacion;
+    private String salida = null;
+    private Button btngrabar;
     private List<Persona> listPerson = new ArrayList<Persona>();
     ArrayAdapter<Persona> arrayAdapterPersona;
 
@@ -37,12 +47,16 @@ public class MainActivity extends AppCompatActivity {
 
 FirebaseDatabase firebaseDatabase;
 DatabaseReference databaseReference;
+    private Environment Envinronment;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btngrabar = (Button)findViewById(R.id.btngrabar);
 
         uid = findViewById(R.id.txt_uidPersona);
         desc = findViewById(R.id.txt_descripcionPersona);
@@ -59,8 +73,51 @@ DatabaseReference databaseReference;
         });
     }
 
+public void Grabar(View view){
+        if(grabacion == null){
+            salida = Envinronment.getExternalStorageDirectory().getAbsolutePath() + "/Grabacion.mp3";
+            grabacion = new MediaRecorder();
+            grabacion.setAudioSource(MediaRecorder.AudioSource.MIC);
+            grabacion.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            grabacion.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+            grabacion.setOutputFile(salida);
 
 
+            try{
+                grabacion.prepare();
+                grabacion.start();
+
+            }catch (IOException e){
+
+            }
+            btngrabar.setBackgroundResource(R.drawable.rec);
+            Toast.makeText(getApplicationContext(), "Grabando", Toast.LENGTH_LONG).show();
+        } else if (grabacion != null) {
+            grabacion.stop();
+            grabacion.release();
+            grabacion = null;
+            btngrabar.setBackgroundResource(R.drawable.stop_rec);
+            Toast.makeText(getApplicationContext(), "Grabacion finalizada", Toast.LENGTH_LONG).show();
+
+        }
+
+}
+
+public void Reproducir(View view){
+        if (salida != null){
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            try{
+                mediaPlayer.setDataSource(salida);
+                mediaPlayer.prepare();
+            } catch(IOException e){
+
+            }
+            mediaPlayer.start();
+            Toast.makeText(getApplicationContext(), "Reproduciendo audio", Toast.LENGTH_LONG).show();
+        } else{
+            Toast.makeText(getApplicationContext(), "Necesita grabar un audio", Toast.LENGTH_LONG).show();
+        }
+}
     private void inicializarFirebase() {
         FirebaseApp.initializeApp(this);
         firebaseDatabase = FirebaseDatabase.getInstance();
